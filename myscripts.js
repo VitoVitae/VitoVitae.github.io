1e84ea2af76385fa33c1b62e2da630a34e802a97
VitoVitae <93656950+VitoVitae@users.noreply.github.com>
2024-06-17 00:42:42 +0200

    Parcours et js

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+/*does not work */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
